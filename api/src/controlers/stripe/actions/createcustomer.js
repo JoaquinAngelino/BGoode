74a19075f0bfc22572c5ed68bb139740f411e6cd
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2022-10-27 00:28:34 -0300

    llll

--- api/src/controlers/stripe/actions/createcustomer.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
-
-stripe.customers.create({
-    description: 'My First Test Customer (created for API)',
-})
-    .then(customer => customer)
-    .catch(err => err.message)
